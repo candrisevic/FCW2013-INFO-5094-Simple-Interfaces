f2f6dcc1b6fb31278510e54b9d79754e254ba76c
Andrew <andrew@ubuntu.ubuntu-domain>
2013-02-20 17:29:06 -0500

    Test Cases

--- tests/Data/NodeTest.php
+++ tests/Data/NodeTest.php
@@ -9,4 +9,30 @@ class NodeTest extends \PHPUnit_Framework_TestCase
         $this->assertEquals(0, $node->getKey());
         $this->assertEquals('value', $node->getValue());
     }
+
+    public function testGetKey()
+    {
+        $node = new \GroupC\Data\LinkedNode(0, 'value');
+        $this->assertEquals(0, $node->getKey());
+    }
+
+   public function testGetValue()
+   {
+        $node = new \GroupC\Data\LinkedNode(0, 'value');
+        $this->assertEquals('value', $node->getValue());
+   }
+
+   public function testSetKey() 
+   {
+        $node = new \GroupC\Data\LinkedNode(0, 'value');
+        $node->setKey(10);
+        $this->assertEquals(10, $node->getKey());
+   }
+
+   public function testSetValue() 
+   {
+        $node = new \GroupC\Data\LinkedNode(0, 'value');
+        $node->setValue('newValue');
+        $this->assertEquals('newValue', $node->getValue());
+   }
 }
