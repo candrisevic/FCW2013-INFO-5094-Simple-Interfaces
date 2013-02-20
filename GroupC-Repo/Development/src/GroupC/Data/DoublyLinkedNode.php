6dac23e27631b607db01bd76022133c493c883b1
Chris <chris@ubuntu.(none)>
2013-02-19 23:07:59 -0500

    Class Files Commented

--- GroupC-Repo/Development/src/GroupC/Data/DoublyLinkedNode.php
+++ GroupC-Repo/Development/src/GroupC/Data/DoublyLinkedNode.php
@@ -6,16 +6,42 @@ namespace GroupC\Data;
  */
 defined('IN_LIB') or die;
 
-class DoublyLinkedNode extends \GroupC\Data\LinkedNode implements \GroupC\Data\IDoublyLinkedNode
-{
+   /*
+    *@package Data
+    *@version 1.0
+    *@author Group C
+    *@copyright Group C 2013
+    */
 
+class DoublyLinkedNode extends \GroupC\Data\LinkedNode implements \GroupC\Data\IDoublyLinkedNode
+{    
+    /*
+     *@access private
+     *@var ILinkedNode
+     */
+    
     private $previous = null;
+    
+    /*
+     * Returns the previously linked node.
+     *
+     * @access public
+     * @return IDoublyLinkedNode|null Returns the previously linked node. Will return null
+     *  if no previous node exists.
+     */
 
     public function getPrevious()
     {
         return isset($this->previous) ? $this->previous : null;
     }
-
+    
+    /*
+     * Sets the previous node.
+     *
+     * @access public
+     * @param IDoublyLinkedNode The previously linked node.
+     */
+    
     public function setPrevious(IDoublyLinkedNode &$previous)
     {
         //throw exceptions
--- GroupC-Repo/Development/src/GroupC/Data/Node.php
+++ GroupC-Repo/Development/src/GroupC/Data/Node.php
@@ -6,10 +6,27 @@ namespace GroupC\Data;
  */
 defined('IN_LIB') or die;
 
+   /*
+    *@package Data
+    *@version 1.0
+    *@author Group C
+    *@copyright Group C 2013
+    */
+
 class Node implements \GroupC\Data\INode
 {
-
+     /*
+     *@access private
+     *@var INode
+     */
+     
     private $key;
+    
+     /*
+     *@access private
+     *@var INode
+     */
+     
     private $value;
 
     public function __construct($key, $value)
@@ -17,22 +34,50 @@ class Node implements \GroupC\Data\INode
         $this->setKey($key);
         $this->setValue($value);
     }
-
+    
+    /*
+     * Returns the key value for this node.
+     *
+     * @access public
+     * @return mixed Returns the key value.
+     */
+    
     public function getKey()
     {
         return $this->key;
     }
-
+    
+    /*
+     * Sets the key value for this node.
+     *
+     * @access public
+     * @param mixed The key value.
+     */
+      
     public function setKey($key)
     {
         //throw exceptions
         $this->key = $key;
     }
-
+    
+     /*
+     * Returns the value of this node (the real value assigned).
+     *
+     * @access public
+     * @return mixed The value.
+     */
+     
     public function getValue()
     {
         return $this->value;
     }
+    
+    /*
+     * Sets the value for this node.
+     *
+     * @access public
+     * @param mixed The value.
+     */
     
     public function setValue($value)
     {
